920362e1cbd6ed7a702ee162bed9ad0f417e32f1
Cameron <mooreheadc@hotmail.com>
2016-11-09 18:49:19 -0800

    Fixed html-webpack-plugin to run properly

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: __dirname + '/app/index.html',
     filename: 'index.html',
