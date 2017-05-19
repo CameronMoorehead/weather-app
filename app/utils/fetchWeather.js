b4bce7a9b3a0206c692149c1fa62cfc121303c02
Cameron <mooreheadc@hotmail.com>
2017-05-18 18:07:54 -0700

    fixed 404 on api response

--- app/utils/fetchWeather.js
+++ app/utils/fetchWeather.js
@@ -2,7 +2,7 @@ const id = 'c67db20a5c4fc00e6c7ca20ec59ff6f3'
 const url = 'http://api.openweathermap.org/data/2.5'
 
 const fetchWeather = function(city) {
-    let forecast = `${url}/forecast/daily?q=${city}&units=imperial&cnt=67&appid=${id}`
+    let forecast = `${url}/forecast/daily?q=${city}&units=imperial&cnt=7&appid=${id}`
 
     return fetch(forecast).then((response) => response.json())
 }
